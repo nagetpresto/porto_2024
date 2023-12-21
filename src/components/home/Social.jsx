f083efb95ee3d8d203bcf8bbe9becd23bcb9a1eb
bilqistwork <bilqist@elistec.com>
2023-12-22 00:12:56 +0700

    Initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/bilqistaz/" className="home__social-icon" target='_blank'>
+          <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/bilqistaz/" className="home__social-icon" target='_blank'>
+          <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/nagetpresto/" className="home__social-icon" target='_blank'>
+          <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
